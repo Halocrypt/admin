0c7f024827d5242074379c2e124152c304c2cc11
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-04-19 22:13:23 +0530

    new logo

--- frontend/src/components/Icons/Halo.tsx
+++ frontend/src/components/Icons/Halo.tsx
@@ -8,108 +8,113 @@ export function HaloIcon({
   return (
     <svg
       fill="none"
-      viewBox="0 0 71 125"
+      viewBox="0 0 261 460"
+      className={className}
       height={size}
       width={size}
-      className={className}
     >
-      <g clip-path="url(#G)">
+      <g clip-path="url(#H)">
         <path
           fill="url(#A)"
-          d="M17.391 39.454L4.06 15.845a.69.69 0 0 1 .278-.833L28.501.013c.116-.071.448.194.555.278.083-.278.546-.317.833-.278l40.549 5.555c.186.028.516.094.555.278a.64.64 0 0 1-.278.555l-52.77 32.774a.38.38 0 0 1-.204.023l-.074.254c-.023.008-.047.01-.071.007l-.207-.007zm0 0z"
+          d="M63.931 145.19L14.925 58.31c-.213-.55-.228-1.157-.042-1.717a2.53 2.53 0 0 1 1.063-1.349L104.77.049c.426-.263 1.646.715 2.042 1.022.306-1.022 2.007-1.168 3.063-1.022l149.062 20.442c.683.102 1.898.346 2.042 1.022.016.399-.069.797-.247 1.154s-.445.664-.774.89L65.973 144.167a1.39 1.39 0 0 1-.748.086l-.274.937a.59.59 0 0 1-.26.026l-.76-.026zm0 0c.007.019 0 .026 0 .026s-.043-.026 0-.026z"
         />
         <path
           fill="url(#B)"
-          d="M4.06 15.289L.172 76.95c-.005.061-.059.262 0 .278s-.025-.222 0-.278l17.22-37.496"
+          d="M14.925 56.265L.631 283.174c-.019.224-.217.966 0 1.022s-.091-.817 0-1.022l63.3-137.985"
         />
         <path
           fill="url(#C)"
-          d="M.449 77.783L37.944 91.67l8.887-28.053L.172 76.672c-.449.882.108 1.042.278 1.111z"
+          d="M1.652 286.24l137.831 51.106 32.671-103.233L.631 282.152c-1.652 3.247.397 3.833 1.021 4.088z"
         />
         <path
           fill="url(#D)"
-          d="M49.331 124.722L.171 77.505l38.05 13.054 11.387 34.163c-.049.022-.102.032-.155.03a.19.19 0 0 1-.122-.03zm.278 0c.025-.015.031-.004.029.007-.019.014-.024.009-.029-.007zm-31.94-85.546L69.883 6.957a.62.62 0 0 1 .555 0c.111.096.058.141 0 .278L46.553 64.451 17.669 39.176z"
+          d="M170.118 236.157l12.236 217.71c.016.239.256.981 0 1.023s-1.039.229-1.112 0l-43.381-121.632 32.257-97.101z"
         />
         <path
           fill="url(#E)"
-          d="M46.277 64.173l3.328 59.161c.005.064.07.266 0 .277s-.282.062-.302 0L37.502 90.559l8.775-26.386z"
+          d="M259.957 23.557L180.96 458.978a4.43 4.43 0 0 1-1.054 1.022c-.13 0 .026-.897 0-1.022L168.32 240.245l91.637-216.688z"
         />
+        <path fill="url(#F)" d="M64.886 145L0 284l168-48.675L64.886 145z" />
         <path
-          fill="url(#F)"
-          d="M70.716 6.401L49.227 124.722c-.08.108-.176.202-.287.278-.036 0 .007-.244 0-.278l-3.152-59.438L70.716 6.401z"
+          fill="url(#G)"
+          d="M181.342 458.978L.63 285.218l139.873 48.04 41.86 125.72c-.179.08-.375.118-.571.11-.158.016-.317-.023-.45-.11zm1.021 0c.091-.056.114-.015.107.025s-.024.052-.047.052-.041-.019-.06-.077zM64.951 144.166L256.894 25.601c.317-.158.666-.241 1.021-.241s.704.082 1.021.241c.408.354.211.52 0 1.022l-87.804 210.556-106.181-93.013z"
         />
       </g>
       <defs>
         <linearGradient
           id="A"
-          x1="37.504"
-          x2="37.504"
-          y1="0"
-          y2="39.462"
-          xlinkHref="#H"
+          x1="137.867"
+          x2="137.867"
+          y1=".001"
+          y2="145.22"
+          xlinkHref="#I"
         >
           <stop stop-color="#6a11cb" stop-opacity=".161" />
           <stop offset="1" stop-color="#2575fc" stop-opacity=".831" />
         </linearGradient>
         <linearGradient
           id="B"
-          x1="8.768"
-          x2="8.768"
-          y1="15.289"
-          y2="77.228"
-          xlinkHref="#H"
+          x1="32.231"
+          x2="32.231"
+          y1="56.265"
+          y2="284.199"
+          xlinkHref="#I"
         >
           <stop stop-color="#2575fc" stop-opacity="0" />
           <stop offset="1" stop-color="#6a11cb" />
         </linearGradient>
         <linearGradient
           id="C"
-          x1="23.416"
-          x2="23.416"
-          y1="63.618"
-          y2="91.67"
-          xlinkHref="#H"
+          x1="86.078"
+          x2="86.078"
+          y1="234.113"
+          y2="337.346"
+          xlinkHref="#I"
         >
           <stop stop-color="#6a11cb" />
           <stop offset="1" stop-color="#2575fc" stop-opacity=".4" />
         </linearGradient>
         <linearGradient
           id="D"
-          x1="35.337"
-          x2="35.337"
-          y1="6.891"
-          y2="124.753"
-          xlinkHref="#H"
-        >
-          <stop stop-color="#6a11cb" stop-opacity=".22" />
-          <stop offset="1" stop-color="#2575fc" />
-        </linearGradient>
-        <linearGradient
-          id="E"
-          x1="43.57"
-          x2="43.57"
-          y1="64.173"
-          y2="123.642"
-          xlinkHref="#H"
+          x1="160.166"
+          x2="160.166"
+          y1="236.157"
+          y2="455.001"
+          xlinkHref="#I"
         >
           <stop stop-color="#6a11cb" />
           <stop offset="1" stop-color="#2575fc" stop-opacity=".259" />
         </linearGradient>
         <linearGradient
-          id="F"
-          x1="58.252"
-          x2="58.252"
-          y1="6.401"
-          y2="125"
-          xlinkHref="#H"
+          id="E"
+          x1="214.139"
+          x2="214.139"
+          y1="23.557"
+          y2="460"
+          xlinkHref="#I"
         >
           <stop stop-color="#6a11cb" />
           <stop offset="1" stop-color="#2575fc" stop-opacity=".02" />
         </linearGradient>
-        <clipPath id="G">
-          <path fill="#fff" d="M0 0h71v125H0z" />
+        <linearGradient id="F" x1="84" x2="84" y1="145" y2="284" xlinkHref="#I">
+          <stop stop-color="#588dfa" />
+          <stop offset="1" stop-color="#210c4b" />
+        </linearGradient>
+        <linearGradient
+          id="G"
+          x1="129.902"
+          x2="129.902"
+          y1="25.361"
+          y2="459.092"
+          xlinkHref="#I"
+        >
+          <stop stop-color="#6a11cb" stop-opacity=".22" />
+          <stop offset="1" stop-color="#2575fc" />
+        </linearGradient>
+        <clipPath id="H">
+          <path fill="#fff" d="M0 0h261v460H0z" />
         </clipPath>
-        <linearGradient id="H" gradientUnits="userSpaceOnUse" />
+        <linearGradient id="I" gradientUnits="userSpaceOnUse" />
       </defs>
     </svg>
   );
