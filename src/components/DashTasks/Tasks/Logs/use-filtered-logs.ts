e07edf2693b8eb17170204e1346a78ae572b099e
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-06-02 02:06:11 +0530

    search by answer as well

--- src/components/DashTasks/Tasks/Logs/use-filtered-logs.ts
+++ src/components/DashTasks/Tasks/Logs/use-filtered-logs.ts
@@ -15,7 +15,7 @@ export function useFilteredLogs(
       if (!logs) return;
       setFilteredLogs(
         logs.filter((x) => {
-          const f = !query || contains(x[0], query);
+          const f = !query || contains(x[0], query) || contains(x[2], query);
           if (filterType === "all") return f;
           return f && (filterType === "correct" ? x[3] : !x[3]);
         })
