6e06f18400d110c8da0729678cfcfab66819aebe
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-06-02 11:22:59 +0530

    fix

--- src/components/DashTasks/Tasks/Logs/use-filtered-logs.ts
+++ src/components/DashTasks/Tasks/Logs/use-filtered-logs.ts
@@ -18,7 +18,10 @@ export function useFilteredLogs(
       if (!logs) return;
       setFilteredLogs(
         logs.filter((x) => {
-          let f = !query || contains(x[0], query) || contains(x[2], query);
+          let f =
+            (!query && !cleanUser) ||
+            contains(x[0], query) ||
+            contains(x[2], query);
           if (cleanUser) {
             f = x[0] === cleanUser;
           }
